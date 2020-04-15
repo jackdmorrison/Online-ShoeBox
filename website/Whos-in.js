322f3c577ccef05cca203968e71c3ab5c10cc7db
jack morrison <55364225+jackdmorrison@users.noreply.github.com>
2020-04-15 19:09:32 +0100

    comments

--- website/Whos-in.js
+++ website/Whos-in.js
@@ -1,12 +1,15 @@
-/*this function takes in a url for the api you want to use and takes in an image, the one you want display when a person is in*/
+/*this function takes in a url for the api you want to use and takes in an imagesrc and image id, and changes the src of the image based on the reults of the api fetch*/
 
 function getData(url,empty,imgID){
+  //finds the element with that iD
   var image= document.getElementById(imgID);
-  //var theDiv= document.getElementById("Image-Here")
 
+//fetches info from api
   fetch(url)
+  //turns result into .json()
       .then((resp) => resp.json())
       .then(function(data) {
+        //stringifys the data and turns it from string to array then to int until 
         console.log(data);
         var string=JSON.stringify(data);
         console.log(data);
@@ -25,5 +28,4 @@ function getData(url,empty,imgID){
       .catch(function(error) {
         console.log(error);
       });
-  //theDiv.appendChild(image);
 }
